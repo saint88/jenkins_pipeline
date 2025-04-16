ca1f6a79c5c131ec37d1adcfa4c824a2bbf911ea
Pavel <svyatoj1988@gmail.com>
2025-04-16 17:14:23 +0000

    Add some fixes

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -11,46 +11,6 @@ timeout(300) {
             }
         }
 
-        stage('Running tests') {
-            exitCode = sh(
-                script: 'sudo docker run --rm --name=ui_tests -t localhost:5005/ui_tests:1.0.0',
-                returnStatus: true
-            )
-            
-            if(exitCode > 0) {
-                currentBuild.status = 'UNSTABLE'
-            }
-        }
-
-        stage('Work with regexp') {
-            def matcher = """test 12 string
-            test 12 string
-            """ =~ /.*?(\d+).*/
-            echo matcher[0][1]
-            echo matcher[1][1]
-
-            def map = [:]
-            map['key1'] = 'val1'
-            map['key2'] = 'val2'
-
-            map.each {k, v -> {
-                echo "key=$k"
-                echo "val=$v"
-            }}
-        }
-
-        script('Publish allure report') {
-            dir('api-tests') {
-                allure([
-                    includeProperties: false,
-                    jdk: '',
-                    properties: [],
-                    reportBuildPolicy: 'ALWAYS',
-                    results: [[path: './allure-results']]
-                ])
-            }
-        }
-
         def configScriptPath = './config/config.py'
 
         stage('Generate job.ini config') {
